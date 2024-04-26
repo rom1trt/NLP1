cfbb763776753692bc94f2bd86de786279c26b31
Lina Farchado <lina.farchado@epita.fr>
2024-04-26 19:56:42 +0200

    feat(tokenizer+vectorizer): updated both in config 1 and 2

--- configuration2/tokenizer.py
+++ configuration2/tokenizer.py
@@ -2,8 +2,9 @@ import pandas as pd
 import tiktoken
 
 def tokenizer(X):
-    X_strings = " ".join(X)
     enc = tiktoken.encoding_for_model("gpt-4")
-    enc.encode("hello world")
-    X_tokens = enc.encode(X_strings)
-    return X_tokens
+    tokenized_documents = []
+    for text in X:
+        tokenized_document = enc.encode(text)
+        tokenized_documents.append(tokenized_document)
+    return tokenized_documents
--- configuration2/vectorizer.py
+++ configuration2/vectorizer.py
@@ -1,8 +1,21 @@
-import sklearn as sk
+import sklearn.feature_extraction.text as sk_text
 
-# tf-idf vectorizer
 def vectorizer(tokenized_documents):
-    ## tokenized_documents is a list of lists, where each inner list contains tokens
-    vectorizer = sk.feature_extraction.text.TfidfVectorizer(lowercase=False, preprocessor=None, tokenizer=lambda x: x)
-    X = vectorizer.fit_transform(tokenized_documents)
+    # Conversion des bytes en chaînes de caractères si nécessaire
+    tokenized_documents_str = []
+    for document in tokenized_documents:
+        if isinstance(document, list):
+            # Convertir les bytes en chaînes de caractères
+            tokenized_document_str = [token.decode('utf-8') if isinstance(token, bytes) else str(token) for token in document]
+            tokenized_documents_str.append(tokenized_document_str)
+        else:
+            # Si le document n'est pas une liste, le convertir en liste de chaîne de caractères
+            tokenized_documents_str.append([str(document)])
+
+    # Initialiser le vectoriseur
+    vectorizer = sk_text.CountVectorizer(lowercase=False, preprocessor=None, tokenizer=lambda x: x)
+    
+    # Adapter et transformer les données
+    X = vectorizer.fit_transform(tokenized_documents_str)
+    
     return X, vectorizer
