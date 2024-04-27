ef381db5ff6ccd6663fe8fe82e992e5587552a1c
Florian Tigoulet <flotig77@gmail.com>
2024-04-27 17:44:23 +0200

    feat: stop words

--- configuration2/tokenizer.py
+++ configuration2/tokenizer.py
@@ -1,16 +1,27 @@
 import pandas as pd
 import tiktoken
+import nltk
+from nltk.corpus import stopwords
+
+nltk.download('stopwords')
 
 def tokenizer(X):
     enc = tiktoken.encoding_for_model("gpt-4")
+    stop_words = set(stopwords.words('english'))
     num_tokens = 0
     num_sentences = 0
     tokenized_documents = []
     for text in X:
         tokenized_document = enc.encode(text)
-        num_tokens += len(tokenized_document)
+
+        filtered_tokens = [token for token in tokenized_document if token not in stop_words]
+
+        num_tokens += len(filtered_tokens)
         num_sentences += text.count('.') + text.count('!') + text.count('?')
-        tokenized_documents.append(tokenized_document)
+        
+        tokenized_documents.append(filtered_tokens)
+
     print("Number of tokens: ", num_tokens)
     print("Number of sentences: ", num_sentences)
+    
     return tokenized_documents
--- configuration3/tokenizer.py
+++ configuration3/tokenizer.py
@@ -1,19 +1,25 @@
 import pandas as pd
 import spacy
+import nltk
+from nltk.corpus import stopwords
+
+nltk.download('stopwords')
 
 def tokenizer(X):
     nlp = spacy.load("en_core_web_sm")
-
+    stop_words = set(stopwords.words('english'))
     num_tokens = 0
     num_sentences = 0
     tokenized_data = []
-    for doc in nlp.pipe(X, batch_size=5, disable=["parser", "ner"]):
-        tokens = [token.text for token in doc]
-        num_tokens += len(tokens)
-        num_sentences += len(list(doc.sents))
-        tokenized_data.append(tokens)
     
+    for doc in nlp.pipe(X, batch_size=5, disable=["parser", "ner"]):
+        tokens = [token.text for token in doc if token.text not in stop_words]
+        #num_tokens += len(tokens)
+        #num_sentences += len(list(doc.sents))
+        tokenized_data.append(tokens)
+
     print("Number of tokens: ", num_tokens)
     print("Number of sentences: ", num_sentences)
     
     return tokenized_data
+
